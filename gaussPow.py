743a186b81ad78cf85c95f09370706b517704435
DaviddeO <56536143+DaviddeO@users.noreply.github.com>
2020-03-11 14:27:39 +0000

    Script to plot heatmap

---
+++ gaussPow.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+numPoints = 201
+dt = 0.001
+sigma2X = 0.005
+sigma2Y = 0.005
+
+x = np.linspace(-1, 1, numPoints)
+y = np.linspace(-1, 1, numPoints)
+points = np.zeros((numPoints, numPoints))
+
+t = np.arange(0, 2 * np.pi, dt)
+xt = np.sin(13 * t)
+yt = np.sin(14 * t)
+
+
+for i in range(len(x)):
+    expX = (x[i] - xt)**2 / sigma2X
+    for j in range(len(y)):
+        expY = (y[j] - yt)**2 / sigma2Y
+        pwr = np.exp(-0.5 * (expX + expY))
+        points[j][i] = np.sum(pwr)
+
+fig, ax = plt.subplots()
+ax.imshow(points, interpolation="bilinear", origin="lower")
+ax.set_axis_off()
+plt.show()
